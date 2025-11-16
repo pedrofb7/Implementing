dfad002ae0c01b5863402f95d8462ba8c080c839
JpOuro <joaopedro06vieiraalmeida@gmail.com>
2025-11-16 19:17:02 -0300

    Função F1

---
+++ queue.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// node
+typedef struct node {
+    int val;
+    struct node *next;
+} node;
+
+// queue
+typedef struct {
+    node *start;
+    node *end;
+    int size;
+} queue;
+
+// criando a fila
+void create(queue *Q) {
+    Q->start = NULL;
+    Q->end = NULL;
+    Q->size = 0;
+}
+
+// adicionar à fila
+void enqueue(queue *Q, int x) {
+    node *new = (node *)malloc(sizeof(node));
+    new->val = x;
+    new->next = NULL;
+
+    // checando se a fila está vazia
+    if (Q->start == NULL) {
+        Q->start = new;
+        Q->end = new;
+
+    } else {
+        Q->end->next = new; // definindo new como o poximo elemento da fila
+        Q->end = new;       // definindo new como o novo end
+    }
+
+    Q->size++;
+}
+
+int dequeue(queue *Q) {
+    if (Q->start == NULL) return -1;
+
+    node *temp = Q->start;
+    int res = temp->val;
+
+    Q->start = Q->start->next;
+
+    // se fosse o ultimo elemento da fila torna end nulo
+    if (Q->start == NULL) {
+        Q->end = NULL;
+    }
+    Q->size--;
+
+    free(temp);
+
+    return res;
+}
+
+int main () {
+    queue *fila;
+    create(fila);
+    int q;
+    scanf("%d", &q);
+    while (q--) {
+        char a;
+        scanf("%c", &a);
+            switch (a) {
+            case 'I':
+                int x;
+                scanf("%d", &x);
+                enqueue(fila, x);
+                break;
+            
+            case 'R':
+                printf("%d", dequeue(fila));
+                break;
+            
+            case 'F':
+                if (fila->start == NULL) {
+                    printf("%d\n", -1);
+                }
+                else {
+                    printf("%d", fila->start);
+                }
+                break;
+            
+            case 'B':
+                if (fila->end == NULL) {
+                    printf("%d\n", -1);
+                }
+                else {
+                    printf("%d", fila->end);
+                }
+                break;
+            
+            case 'S':
+                printf("%d", fila->size);
+                break;
+            
+            case 'C':
+                
+                break;
+            
+            case 'P':
+                if (fila->start != NULL) {
+                    node *temp = fila->start;
+                    while (temp == NULL) {
+                        scanf("%d ", temp->val);
+                        temp = temp->next;
+                    }
+                }
+                printf("\n");
+                break;
+            
+            default:
+                break;
+            }
+    }
+
+    return 0;
+}
